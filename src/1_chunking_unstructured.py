be5d48f2b8c1a0d9704a8901e51443d4a0e69a8c
tusharmoon <tusharmoon2014@gmail.com>
2026-03-19 11:02:08 +0530

    D2

---
+++ src/1_chunking_unstructured.py
@@ -0,0 +1,122 @@
+
+from pypdf import PdfReader
+
+reader = PdfReader("data\Executive Summary.pdf")
+
+text = ""
+
+for page in reader.pages:
+    text += page.extract_text()
+
+#print(text)
+
+#---------------------------------------------------------------------------------
+
+# 1. Character Text Splitting
+
+# from langchain_core.documents import Document
+
+# print("#### Character Text Splitting ####")
+
+# # Manual Splitting
+# chunks = []
+# chunk_size = 35 # Characters
+# for i in range(0, len(text), chunk_size):  # range(start,stop,step)
+#     chunk = text[i:i + chunk_size]     #text[0:0+35]
+#     chunks.append(chunk)
+    
+# #print(chunks)
+
+# # for chunk in chunks:
+# #     Document(page_content=chunk, metadata={"source": "local"})
+
+# documents = [Document(page_content=chunk, metadata={"source": "local"}) for chunk in chunks]
+# print(documents)
+
+#---------------------------------------------------------------------------------
+
+
+# # Automatic Text Splitting
+# from langchain_text_splitters import CharacterTextSplitter
+# text_splitter = CharacterTextSplitter(chunk_size = 50, chunk_overlap=10, separator='', strip_whitespace=False)
+# documents = text_splitter.create_documents([text])
+# print(documents)
+
+
+#---------------------------------------------------------------------------------
+
+# 2. Recursive Character Text Splitting
+# print("#### Recursive Character Text Splitting ####")
+
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size = 65, chunk_overlap=10) # ["\n\n", "\n", " ", ""] 65,450
+# documents = text_splitter.create_documents([text])
+# print(documents) 
+
+
+#---------------------------------------------------------------------------------
+
+# # 3. Document Specific Splitting
+# print("#### Document Specific Splitting ####")
+
+# # Document Specific Splitting - Markdown
+# from langchain_text_splitters import MarkdownTextSplitter
+# markdown_text = """
+# # Fun in California
+
+# ## Driving
+
+# Try driving on the 1 down to San Diego
+
+# ### Food
+
+# Make sure to eat a burrito while you're there
+
+# ## Hiking
+
+# Go to Yosemite
+# """
+# splitter = MarkdownTextSplitter(chunk_size = 40, chunk_overlap=0)
+# documents = splitter.create_documents([markdown_text])
+# print(documents)
+
+#---------------------------------------------------------------------------------
+
+# Document Specific Splitting - Python
+# from langchain_text_splitters import PythonCodeTextSplitter
+# python_text = """
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+#     self.code = code
+
+# p1 = Person("John", 36)
+
+# for i in range(10):
+#     print (i)
+# """
+# python_splitter = PythonCodeTextSplitter(chunk_size=120, chunk_overlap=0)
+# documents = python_splitter.create_documents([python_text])
+# print(documents)
+
+#---------------------------------------------------------------------------------
+# 4. Semantic Chunking
+print("#### Semantic Chunking ####")
+
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_huggingface import HuggingFaceEmbeddings
+
+# Local embedding model
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+text_splitter = SemanticChunker(embeddings,breakpoint_threshold_type="percentile")
+
+documents = text_splitter.create_documents([text])
+
+print(documents)
+
+
+
